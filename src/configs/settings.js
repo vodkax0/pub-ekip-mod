58e8450ef8a3c4744e5000b43db36189e29309aa
AnarchyX <100626938+AnarchyX0@users.noreply.github.com>
2022-03-23 19:05:42 +0300

    Update settings.js

--- src/configs/settings.js
+++ src/configs/settings.js
@@ -8,28 +8,28 @@
     //General Settings
     client.settings = {
 
-        Prefix: ".",
-        Token: "",
-        Owners: [],
-        OtherBots: [],
-        VoiceChannel: "",
-        Activity: "LISTENING",
-        Status: "idle",
-        MongoURL: "",
-        Footer: "SeonerVorteX",
-        DisableCooldownsForAdmins: true,
+        Prefix: ".",//prefix
+        Token: "",//botunun tokeni
+        Owners: [],//bot sahibi id
+        OtherBots: [],//yeni rol oluşturun sunucunuzdaki tüm botlara o rolü verin ondan sonra rolün id yi "" kısmına ekleyin
+        VoiceChannel: "",//ses kanal id
+        Activity: "LISTENING",// Listening yerine WATCHING - PLAYING gibi şeyler yazabilirsiniz
+        Status: "dnd",//online idle dnd offline arasından istediğinizi yapabilirsiniz
+        MongoURL: "",//mongodb url'niz
+        Footer: "Anarchy",//bot footeriniz
+        DisableCooldownsForAdmins: true,//elleme knka
 
     };
 
     //Activity Messages
     client.statusMessages = [
 
-        "Only VorteX!",
-        "Created By VorteX!",
-        "discord.gg/MTNkXHnX3b"
+        "Only Anarchy!",//1. durum
+        "Created By Anarchy!",//2. durum
+        "Anarchy"//3. durum
 
     ];
-
+//Emojiler kısmını elleme kanka
     //Emojis
     client.systemEmojis = [
 
@@ -73,105 +73,105 @@
     client.guildSettings = {
 
         ///General
-        guildID: "",
-        guildTags: [],
-        guildDiscriminator: "",
-        guildTeams: [],
-        meetRole: "",
-        meetChannel: "",
-        nameTag: "",
-        dmMessages: true,
-        unAuthorizedMessages: true,
+        guildID: "",//sunucu id
+        guildTags: [],//tagınız
+        guildDiscriminator: "",//etiket tag
+        guildTeams: [],//tag alanlara verilecek rol id
+        meetRole: "",//toplantıya katılanlara verilecek rol id
+        meetChannel: "",//toplantı kanal id (ses kanal)
+        nameTag: "",//isim tag
+        dmMessages: true,//elleme işte olum
+        unAuthorizedMessages: true,//no kanka elleme
 
         ///Staffs
-        staffRoles: [],
-        transporterSpears: [],
-        registerSpears: [],
-        staffGiver: "",
-        botYt: "",
+        staffRoles: [],//ilk yetki id
+        transporterSpears: [],//transporter id
+        registerSpears: [],//kayıt sorumlusu id
+        staffGiver: "",//yetki verici rol id
+        botYt: "",//botlara özel rol açın admin salın ve o rolün id yi ekleyin buraya (ROLÜ DE TÜM BOTLARA VERİN AMQ)
 
         ///Penals
         penals: {
 
             ///Ban
             ban: {
-                staffs: [],
+                staffs: [],//ban yetkilisi id
                 penalPoint: 40,
                 penalLimit: 5,
-                log: "",
+                log: "",//ban log id (alttaki gif linklerini de elleme iyi olanları ekledik oraya amk)
                 banGifs: ['https://media1.tenor.com/images/ed33599ac8db8867ee23bae29b20b0ec/tenor.gif?itemid=14760307', 'https://media.giphy.com/media/fe4dDMD2cAU5RfEaCU/giphy.gif', 'https://media1.tenor.com/images/4732faf454006e370fa9ec6e53dbf040/tenor.gif?itemid=14678194'],
                 unbanGifs: ['https://data.whicdn.com/images/192611812/original.gif'],
             },
 
             ///Jail
             jail: {
-                staffs: [],
-                jailRoles: [],
-                jailChannel: "",
+                staffs: [],//jail yetkilisi rol id
+                jailRoles: [],//jaillenen kişilere verilecek rol id
+                jailChannel: "",//jail chat kanal id
                 penalPoint: 30,
                 penalLimit: 5,
-                log: "",
+                log: "",//jail log id
             },
 
             ///Chat Mute
             chatMute: {
-                staffs: [],
-                cmuteRoles: [],
+                staffs: [],//chat mute yetkilisi id
+                cmuteRoles: [],//chat muted rol id
                 penalPoint: 20,
                 penalLimit: 5,
-                log: "",
+                log: "",//chat mute log id
             },
 
             ///Voice Mute
             voiceMute: {
-                staffs: [],
-                vmuteRoles: [],
+                staffs: [],//ses mute yt rol id
+                vmuteRoles: [],//ses mute alanlara verilecek rol
                 penalPoint: 20,
                 penalLimit: 5,
-                log: "",
+                log: "",//voice mute log id
             },
 
             ///Warn
             warn: {
-                staffs: [],
+                staffs: [],//warn yetkilisi id
                 warnRoles: [{
                     warnCount: 1,
-                    warnRole: "", 
+                    warnRole: "",//1. uyarı rol id
                 }, {
                     warnCount: 2,
-                    warnRole: "",
+                    warnRole: "",//2. uyarı rol id
                 }, {
                     warnCount: 3,
-                    warnRole: "",
+                    warnRole: "",//3. uyarı rol id
                 }],
                 penalPoint: 10,
-                log: "",
+                log: "",//uyarı log
             },
 
         },
 
         ///Registration
         registration: {
-            unregisterName: "",
-            unregisterRoles: [],
-            unregisterChannel: "",
-            quarantineRole: "",
-            familyRole: "",
+            unregisterName: "",//sunucuya biri gelince yapılacak oto isim (örnek: İsim | Yaş ver amk)
+            unregisterRoles: [],//kayıtsız rol id
+            unregisterChannel: "",//register chat id
+            quarantineRole: "",//Karantina rol id
+            familyRole: "",//tag alarak verilecek rol id
         },
 
         ///Forbidden Tag
         forbiddenTag: {
-            forbidRoles: [],
-            forbidChannel: "",
-            forbidLog: "",
+            forbidRoles: [],//yasaklı tag rol id
+            forbidChannel: "",//yasaklı tag chat id
+            forbidLog: "",//yasaklı tag log id
         },
 
         ///Logs
         logs: {
-            pointLog: "",
-            voiceLog: "",
-            messageLog: "",
-            securityLog: "",
+            pointLog: "",//point log id
+            voiceLog: "",//ses log id
+            messageLog: "",//mesaj log id
+            securityLog: "",//security log id
         },
 
     };
